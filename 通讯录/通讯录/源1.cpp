c57b988d02b0b39cdefbe99d7c5bcfb289cbefff
yuqianer1210 <332349624@qq.com>
2019-03-25 23:37:20 +0800

    Update 源1.cpp

--- 通讯录/通讯录/源1.cpp
+++ 通讯录/通讯录/源1.cpp
@@ -8,28 +8,36 @@ Start::Start()
 {}
 void Start::Display()
 {
-	int n;
-	while(true)
+	fstream in("F:\\person.txt");
+	if (!in)
 	{
-	cout << "*********************************************\n";
-	cout << "*               通信录管理系统              *\n";
-	cout << "*                                           *\n";
-	cout << "*               1. 添加通信录               *\n";
-	cout << "*               2. 删除通信录               *\n";
-	cout << "*               3. 显示通信录               *\n";
-	cout << "*               4. 修改通信录               *\n";
-	cout << "*               5. 排序通信录               *\n";
-	cout << "*               6. 查找通信录               *\n";
-	cout << "*               7. 退出                     *\n";
-	cout << "*********************************************\n";
-	cout << "\n请输入(1 ~ 7): ";
-	cin >> n;
-	if(n==1)Insert();
-	if(n==2)Delete();
-	if(n==3)output();
-	if(n==4)Modify();
-	if(n==5)Sort();
-	if(n==6)Search();
-	if(n==7)break;
-	if(n<=0||n>=8)continue;
+		cout << "Cannot open the file\n";
+		return;
 	}
+	int n;
+	while (true)
+	{
+		cout << "*********************************************\n";
+		cout << "*               通信录管理系统              *\n";
+		cout << "*                                           *\n";
+		cout << "*               1. 添加通信录               *\n";
+		cout << "*               2. 删除通信录               *\n";
+		cout << "*               3. 显示通信录               *\n";
+		cout << "*               4. 修改通信录               *\n";
+		cout << "*               5. 排序通信录               *\n";
+		cout << "*               6. 查找通信录               *\n";
+		cout << "*               7. 退出                     *\n";
+		cout << "*********************************************\n";
+		cout << "\n请输入(1 ~ 7): ";
+		cin >> n;
+		if (n == 1)Insert();
+		if (n == 2)Delete();
+		if (n == 3)output();
+		if (n == 4)Modify();
+		if (n == 5)Sort();
+		if (n == 6)Search();
+		if (n == 7)break;
+		if (n <= 0 || n >= 8)continue;
+	}
+	in.close();
+}
